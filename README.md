60d23053899ea4955f3f5d66e6b65de4f9ee1806
PythonPioner <sunnyplaycsgoosnova@gmail.com>
2023-11-21 00:24:35 +0500

    lesson5

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # OOP_Python
 
 
-@calssmethod - method класса
+## @calssmethod - method класса
 
 ![Screenshot_17.png](Screenshot/Screenshot_17.png)
 
-@staticmethod - Статический метод
+![Screenshot_18.png](Screenshot%2FScreenshot_18.png)
+
+![Screenshot_19.png](Screenshot%2FScreenshot_19.png)
+
+
+
+
+
+## @staticmethod - Статический метод
+
+![Screenshot_1.png](Screenshot%2FScreenshot_1.png)
--- lesson5.py
+++ lesson5.py
@@ -9,14 +9,22 @@ class Vector:
         return cls.MIN_CORD <= arg <= cls.MAX_CORD
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = self.y = 0
+        if Vector.validate(x) and Vector.validate(y):
+            self.x = x
+            self.y = y
+        print(self.norm2(self.x, self.y))
 
     # method on class Vector
     def get_cord(self):
         return self.x, self.y
 
+    # Static method
+    @staticmethod
+    def norm2(x, y):
+        return x*x + y*y
 
-v = Vector(1, 2)
-res = Vector.get_cord(v)
-print(res)
+
+v = Vector(1, 20)
+print(Vector.norm2(10, 20))
+
