0070320e61b52c5eb59c6d19d9c4508b5c4cd84a
PythonPioner <sunnyplaycsgoosnova@gmail.com>
2023-11-21 00:46:16 +0500

    pushka

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
 # OOP_Python
 
 
-## @calssmethod - method класса
+## Lesson 1 and 2  >>>
+    1 - Attributes Class and Object 
+    2 - methods [__doc__] and [__dict__]
+    3 - getattr, hasattr, setattr, delattr
+
+![Screenshot_2.png](Screenshot%2FScreenshot_2.png)
+
+![Screenshot_12.png](Screenshot%2FScreenshot_12.png)
+
+![Screenshot_2123.png](Screenshot%2FScreenshot_2123.png)
+
+
+## Lesson 3 and 4 >>>
+    1 - Magic Methods 
+    2 - Initializer and Finalizer
+
+![Screenshot_4.png](Screenshot%2FScreenshot_4.png)
+
+![Screenshot_5.png](Screenshot%2FScreenshot_5.png)
+
+![Screenshot_6.png](Screenshot%2FScreenshot_6.png)
+
+![Screenshot_7.png](Screenshot%2FScreenshot_7.png)
+
+
+
+## Lesson 5 and 6 >>>
+    1 - @calssmethod - method класса
 
 ![Screenshot_17.png](Screenshot/Screenshot_17.png)
 
@@ -13,6 +40,10 @@
 
 
 
-## @staticmethod - Статический метод
+    2 - @staticmethod - Статический метод
 
 ![Screenshot_1.png](Screenshot%2FScreenshot_1.png)
+
+# FUN MOD
+
+![Screenshot_3.png](Screenshot%2FScreenshot_3.png)
--- lesson4.py
+++ lesson4.py
@@ -1,37 +1,40 @@
-# class Point:
-#     def __new__(cls, *args, **kwargs):
-#         """
-#         Магический метод __new__
-#         cls ссылается на class
-#         """
-#         print(f"Вызов __new__  для {str(cls)}")
-#         # возврощаем ссылку в объект класса
-#         return super().__new__(cls)
-#
-#
-#     def __init__(self, x=0, y=0):
-#         """
-#         Магический метод __init__
-#         self ссылается на объект(экзэмпляр)
-#         """
-#         print(f"Вызовт __init__ для {str(self)}")
-#         self.x = x
-#         self.y = y
-#
-#
-# pt = Point(1, 2)
-# print(pt)
+class Point:
+    def __new__(cls, *args, **kwargs):
+        """
+        Магический метод __new__
+        cls ссылается на class
+        """
+        print(f"Вызов __new__  для {str(cls)}")
+        # возврощаем ссылку в объект класса
+        return super().__new__(cls)
+
+
+    def __init__(self, x=0, y=0):
+        """
+        Магический метод __init__
+        self ссылается на объект(экзэмпляр)
+        """
+        print(f"Вызовт __init__ для {str(self)}")
+        self.x = x
+        self.y = y
+
+
+pt = Point(1, 2)
+print(pt)
 
 
 class DataBase:
     __instance = None
 
+    # Magical method __new__ - Работает до создания объекта класса
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
 
         return cls.__instance
 
+    # Magical method __del__ - Работает после удаления объекта в финале
+    # если действий с объектом не происходит
     def __del__(self):
         DataBase.__instance = None
     def __init__(self, user, psw, port):
